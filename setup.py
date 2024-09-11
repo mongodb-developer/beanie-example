dc39df8945fc653ca3d1b801c976bd976bf816da
Mark Smith <judy@judy.co.uk>
2024-09-11 12:18:36 +0100

    Minor setup.py formatting changes.

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 EXTRAS_REQUIRE = {
     "tests": [
+        "coverage[toml]~=5.0.2",
         "pytest",
-        "coverage[toml]>=5.0.2",
     ],
 }
 EXTRAS_REQUIRE["dev"] = EXTRAS_REQUIRE["tests"] + [
@@ -19,12 +19,12 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
-        "fastapi==0.114.0",
-        "beanie==1.26.0",
-        "pydantic == 2.9.1",
-        "pydantic-settings==2.4.0",
-        "motor==3.5.1",
-        "tqdm==4.66.5",
+        "beanie             ~=1.26.0",
+        "fastapi            ~=0.114.0",
+        "motor              ~=3.5.1",
+        "pydantic           ~=2.9.1",
+        "pydantic-settings  ~=2.4.0",
+        "tqdm               ~=4.66.5",
         "uvicorn",
     ],
     extras_require=EXTRAS_REQUIRE,
